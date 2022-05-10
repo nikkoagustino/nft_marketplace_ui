4f9bd3e1b2dc5770bec41bece7ef2976e4c89650
topcoder1208 <topcoder1208@gmail.com>
2022-05-10 11:00:30 -0700

    updated

--- webpack.config.js
+++ webpack.config.js
@@ -9,16 +9,4 @@ module.exports = {
         ],
     },
     ignoreWarnings: [/Failed to parse source map/],
-    resolve: {
-        fallback: {
-            url: require.resolve('url'),
-            assert: require.resolve('assert'),
-            crypto: require.resolve('crypto-browserify'),
-            http: require.resolve('stream-http'),
-            https: require.resolve('https-browserify'),
-            os: require.resolve('os-browserify/browser'),
-            buffer: require.resolve('buffer'),
-            stream: require.resolve('stream-browserify'),
-        }
-    }
 };
